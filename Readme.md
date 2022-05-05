e992fbd62474a7fd0444e34f7cc7f3c4685a15bc
kj-grogu <BHARTIPRAKASHTIWARY@gmail.com>
2022-05-05 00:36:49 -0700

    Solved the problem on leetcode to count most no of consecutive ones

--- Readme.md
+++ Readme.md
@@ -1,5 +1,5 @@
 # Algorithms Problems Java
 
-| # | Problem  | Link | Solution |
+| # | Problem  | Problem Link | Solution |
 |---|-------------------------------------------------|-------|-----------------------------------------------------------------------------------------------------|
 | 0 | <b>MaxConsecutiveOnes</b>  <br>             | [485](https://leetcode.com/problems/max-consecutive-ones/)| [Solution](https://github.com/kj-grogu/Data_Structures_Algorithms_Java/blob/main/src/MaxConsecutiveOnes.java)|
--- src/MaxConsecutiveOnes.java
+++ src/MaxConsecutiveOnes.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class MaxConsecutiveOnes{
 
 // 485. Max Consecutive Ones
@@ -21,9 +19,36 @@ public class MaxConsecutiveOnes{
 // 1 <= nums.length <= 105
 // nums[i] is either 0 or 1.
 
-    public int findMaxConsecutiveOnes(int[] nums) {
-            
+public int findMaxConsecutiveOnes(int[] nums) {
+    int max1Cons = 0;
+    int max1ConsTemp = 0;
+    
+    if(nums == null || nums.length == 0)
+        return 0;
+    //int t;
+    
+    //for(int i = 0; i <= nums.length; i++)
+    for(int i = 0; i < nums.length; i++)
+    {
+        //t = i < nums.length ? nums[i] : 0;
+        
+        //if(t == 1)
+          if(nums[i] == 1)  
+        {
+            ++max1ConsTemp;
+        }
+        else
+        {
+            if(max1ConsTemp > max1Cons)
+                max1Cons = max1ConsTemp;
+            max1ConsTemp = 0;
+        }
     }
+    if(max1ConsTemp > max1Cons)
+        max1Cons = max1ConsTemp;
+    
+    return max1Cons; 
+}
 
     public static void main(String[] args) {
         System.out.println("I am main @>@");
