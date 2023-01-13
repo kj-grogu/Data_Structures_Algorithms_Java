58b2cb4cd9b6f13715604bb58ea76ab0a8cad3ac
kj-grogu <BHARTIPRAKASHTIWARY@gmail.com>
2023-01-12 18:55:43 -0800

    Update SortedLinkedListMerge.java
    update

--- src/SortedLinkedListMerge.java
+++ src/SortedLinkedListMerge.java
@@ -89,6 +89,43 @@
         
     return head;
     }
+    //Another way:
+    // public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    //     if(list1 == null && list2 == null)
+    //         return list1;
+    //     if(list1 == null) 
+    //         return  list2;
+    //     if(list2 == null)
+    //         return list1;
+        
+    //     //ListNode curr1 = list1;
+    //     //ListNode curr2 = list2;
+    //     ListNode temp = new ListNode(-1, null);
+    //     ListNode output = temp;
+    //     while(list1 != null &&  list2 != null){
+    //         if(list1.val <= list2.val){
+    //             temp.next = list1;
+    //             temp = temp.next;
+    //             list1 = list1.next;
+    //         }
+    //         else{
+    //             temp.next = list2;
+    //             temp = temp.next;
+    //             list2 = list2.next;
+    //         }
+    //     }
+    //     while(list1 != null){
+    //         temp.next = list1;
+    //         temp = temp.next;
+    //         list1 = list1.next;
+    //     }
+    //     while(list2 != null){
+    //         temp.next = list2;
+    //         temp = temp.next;
+    //         list2 = list2.next;
+    //     }
+    // return output = output.next;
+    // }
     public static void main(String[] args) {
         
 
